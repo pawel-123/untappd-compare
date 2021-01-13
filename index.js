30f2305d2b5eb913504b0a291922362b2eeb059a
pawel_admin <ptcebula@gmail.com>
2021-01-13 21:01:34 +0100

    feat: Add index.html with a form and redirect to results page using form query params for usernames

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const fetch = require('node-fetch');
+const path = require('path');
 require('dotenv').config();
 
 const generateQueryUrl = (username, limit, offset = 0) => {
@@ -95,9 +96,24 @@ const compareBeers = async (user1, user2) => {
     return commonBeers;
 }
 
-app.get('/', async (request, response) => {
-    const commonBeers = await compareBeers('pawcio', 'birr0nauta');
-    response.send(commonBeers);
+const options = {
+    root: path.join(__dirname, '/')
+}
+
+app.get('/', (request, response) => {
+    response.sendFile('/index.html', options);
+});
+
+app.get('/results', async (request, response) => {
+    const user1 = request.query.user1;
+    const user2 = request.query.user2;
+    const commonBeers = await compareBeers(user1, user2);
+    let beerHtml = `<p>${user1} and ${user2} have ${commonBeers.length} beers in common:</p><ul>`;
+    commonBeers.forEach(beer => {
+        beerHtml += `<li>${beer.beer_name}</li>`
+    })
+    beerHtml += '</ul>';
+    response.send(beerHtml);
 });
 
 const PORT = 3002;
