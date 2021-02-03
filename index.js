7522690b5519a4a866684d7c5a52b294d172643e
Max Stralka <max.stralka@bcgdv.com>
2021-02-03 19:05:05 +0100

    chore: improve templating

--- index.js
+++ index.js
@@ -5,26 +5,27 @@ require('dotenv').config();
 const compareBeers = require('./compareBeers');
 
 const options = {
-    root: path.join(__dirname, '/')
-}
+  root: path.join(__dirname, '/'),
+};
 
-app.get('/', (request, response) => {
-    response.sendFile('/index.html', options);
+app.get('/', (_, response) => {
+  response.sendFile('/index.html', options);
 });
 
 app.get('/results', async (request, response) => {
-    const user1 = request.query.user1;
-    const user2 = request.query.user2;
-    const commonBeers = await compareBeers(user1, user2);
-    let beerHtml = `<p>${user1} and ${user2} have ${commonBeers.length} beers in common:</p><ul>`;
-    commonBeers.forEach(beer => {
-        beerHtml += `<li>${beer.beer_name}</li>`
-    })
-    beerHtml += '</ul>';
-    response.send(beerHtml);
+  const user1 = request.query.user1;
+  const user2 = request.query.user2;
+  const commonBeers = await compareBeers(user1, user2);
+
+  const greeting = `<p>${user1} and ${user2} have ${commonBeers.length} beers in common:</p><ul>`;
+  const beers = commonBeers.map(beer => `<li>${beer.beer_name}</li>`);
+
+  const html = `${greeting}<ul>${beers.join()}</ul>`;
+  response.send(html);
 });
 
 const PORT = 3002;
+
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
