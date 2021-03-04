ae4235281dc2cd7ab7dc5d2020aa9355ad4b0017
pawel_admin <ptcebula@gmail.com>
2021-03-04 15:31:22 +0100

    feat: Define userSchema and connect it to comparisonSchema

--- models/comparison.js
+++ models/comparison.js
@@ -1,9 +1,27 @@
 const mongoose = require('mongoose')
 
 const comparisonSchema = new mongoose.Schema({
-    untappdUsers: [String],
+    untappdUsers: {
+        type: [String],
+        minLength: 2,
+        maxLength: 2,
+    },
     commonBeers: [],
     date: Date,
+    users: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User'
+        }
+    ]
+})
+
+comparisonSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString()
+        delete returnedObject._id
+        delete returnedObject.__v
+    }
 })
 
 module.exports = mongoose.model('Comparison', comparisonSchema)
---
+++ models/user.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose')
+
+const userSchema = new mongoose.Schema({
+    username: String,
+    passwordHash: String,
+    savedComparisons: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Comparison'
+        }
+    ]
+})
+
+userSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString()
+        delete returnedObject._id
+        delete returnedObject.__v
+        delete returnedObject.passwordHash
+    }
+})
+
+module.exprts = mongoose.model('User', userSchema)
