c0a5b19e3378dbecdee7d4acd4b19f8cc434c954
pawel_admin <ptcebula@gmail.com>
2021-03-04 15:58:27 +0100

    refactor: Move comparison routes to comparisonRouter

--- index.js
+++ index.js
@@ -3,8 +3,7 @@ const app = express();
 const path = require('path');
 const mongoose = require('mongoose')
 require('dotenv').config();
-const compareBeers = require('./compareBeers');
-const Comparison = require('./models/comparison')
+const comparisonsRouter = require('./controllers/comparisons')
 
 const options = {
     root: path.join(__dirname, '/'),
@@ -23,48 +22,7 @@ app.get('/', (_, response) => {
     response.sendFile('/index.html', options);
 });
 
-app.get('/api/comparison', async (request, response) => {
-    const { user1, user2 } = request.query
-
-    const findComparison = (userA, userB) => Comparison.findOne({ untappdUsers: [userA, userB] })
-
-    const existingComparison = await findComparison(user1, user2) || await findComparison(user2, user1)
-
-    let commonBeers;
-
-    if (!existingComparison) {
-        commonBeers = await compareBeers(user1, user2);
-
-        const comparison = new Comparison({
-            untappdUsers: [user1, user2],
-            commonBeers: commonBeers,
-            date: new Date()
-        })
-
-        await comparison.save()
-        console.log('fetched commonBeers from Untappd and saved to database')
-    } else {
-        commonBeers = existingComparison.commonBeers
-        console.log('fetched commonBeers from database')
-    }
-
-    const greeting = `<p>${user1} and ${user2} have ${commonBeers.length} beers in common:</p><ul>`;
-    const beers = commonBeers.map(beer => `<li>${beer.beer_name}</li>`);
-
-    const html = `${greeting}<ul>${beers.join('')}</ul>`;
-
-    response.send(html);
-});
-
-app.get('/api/comparisons', async (request, response) => {
-    const comparisons = await Comparison.find({})
-
-    const table = `<h1>There are ${comparisons.length} comparisons in the database</h1><table><thead><tr><th>User 1</th><th>User 2</th><th>Common beers</th></tr></thead><tbody>`
-    const beerRows = comparisons.map(comp => `<tr><td>${comp.untappdUsers[0]}</td><td>${comp.untappdUsers[1]}</td><td>${comp.commonBeers.length}</td></tr>`)
-    const html = `${table}${beerRows.join('')}</tbody></table>`
-
-    response.send(html)
-})
+app.use('/api/comparisons', comparisonsRouter)
 
 const PORT = 3002;
 
